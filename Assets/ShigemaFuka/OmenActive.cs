4cca221af238b87089d590a80a4ca09fea037af0
Shigema <tsubakyun4869@gmail.com>
2023-07-17 13:41:26 +0900

    [update]お面の生成

---
+++ Assets/ShigemaFuka/OmenActive.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OmenActive : MonoBehaviour
+{
+    [SerializeField] GameObject _omen;
+    float _time;
+    [SerializeField] float _interval;
+    [SerializeField, Tooltip("消えてからの再生成までの最小値")] int _min;
+    [SerializeField, Tooltip("消えてからの再生成までの最大値")] int _max;
+    [SerializeField, Tooltip("アニメーションの生存時間")] float _lifeTime = 5f;
+    float _animTime;
+
+    private void Start()
+    {
+        //_omen = GetComponent<GameObject>();
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+        if (_time > _interval && GameManager.Instance._isOmenSpawnnig == false)
+        {
+            _omen.SetActive(true);
+            GameManager.Instance._isOmenSpawnnig = true;
+        }
+        if (GameManager.Instance._isOmenSpawnnig == true)
+        {
+            _animTime += Time.deltaTime;
+        }
+        if (_animTime > _lifeTime)
+        {
+            Debug.Log(_animTime);
+            _animTime = 0;
+            _time = 0;
+            _omen.SetActive(false);
+            GameManager.Instance._isOmenSpawnnig = false;
+            _interval = Random.Range(_min, _max + 1);
+        }
+    }
+}
